71ae16d965df25b992ded0832cff135d1fd62721
Theo <octheo.quennehen@gmail.com>
2024-11-06 17:12:34 +0100

    exceptions

--- UniversiteDomain/DataAdapters/IParcoursRepository.cs
+++ UniversiteDomain/DataAdapters/IParcoursRepository.cs
@@ -4,4 +4,8 @@ namespace UniversiteDomain.DataAdapters;
 
 public interface IParcoursRepository: IRepository<Parcours>
 {
+    Task<Parcours> AddEtudiantAsync(Parcours parcours, Etudiant etudiant);
+    Task<Parcours> AddEtudiantAsync(long idParcours, long idEtudiant);
+    Task<Parcours> AddEtudiantAsync(Parcours ? parcours, List<Etudiant> etudiants);
+    Task<Parcours> AddEtudiantAsync(long idParcours, long[] idEtudiants);
 }
