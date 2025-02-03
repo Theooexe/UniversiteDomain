058ef60de79a49b62bd5770a92c81995df15212b
Theoode <theoquennehenpro@gmail.com>
2025-02-03 16:11:40 +0100

    Enitite note

--- UniversiteDomain/Entities/Notes.cs
+++ UniversiteDomain/Entities/Notes.cs
@@ -3,16 +3,16 @@
     public class Notes
     {
         public long Id { get; set; }
-
         public float Valeur { get; set; }
 
         // Association avec l'étudiant
         public long EtudiantId { get; set; }
-        public Etudiant Etudiant { get; set; }  // Navigation vers l'entité Etudiant
+        public Etudiant Etudiant { get; set; }  
 
-       
+        // Association avec l'Ue
         public long UeId { get; set; }
-        public Ue Ue { get; set; }  
+        public Ue Ue { get; set; }  // Navigation vers l'entité Ue
+
         
     }
 }
