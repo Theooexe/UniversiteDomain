37ea0cdc6cf9d16fb0ecc69527f9f65d2d197ddd
Theoode <theoquennehenpro@gmail.com>
2025-02-03 12:41:27 +0100

    modif

--- UniversityEFDataProvider/Data/UniversiteDbContext.cs
+++ UniversityEFDataProvider/Data/UniversiteDbContext.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using UniversiteDomain.Entities;
 
-namespace UniversityEFDataProvider.Data;
+namespace UniversiteEFDataProvider.Data;
  
 public class UniversiteDbContext : DbContext
 {
@@ -24,6 +25,64 @@ public class UniversiteDbContext : DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Propriétés de la table Etudiant
+        // Clé primaire
         base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Etudiant>()
+            .HasKey(e => e.Id);
+        // ManyToOne vers parcours
+        modelBuilder.Entity<Etudiant>()
+            .HasOne(e => e.ParcoursSuivi)
+            .WithMany(p => p.Inscrits);
+        // OneToMany vers Note
+        modelBuilder.Entity<Etudiant>()
+            .HasMany(e => e.NotesObtenues)
+            .WithOne(n => n.Etudiant);
+        
+        // Propriétés de la table Parcours
+        // Clé primaire
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Parcours>()
+            .HasKey(p => p.Id);
+        // ManyToOne vers Etudiant
+        modelBuilder.Entity<Parcours>()
+            .HasMany(p => p.Inscrits)
+            .WithOne(e => e.ParcoursSuivi);
+        // ManyToMany vers Ue
+        modelBuilder.Entity<Parcours>()
+            .HasMany(p => p.UesEnseignees)
+            .WithMany(ue => ue.EnseigneeDans);
+
+        // Propriétés de la table Ue
+        // Clé primaire
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Ue>()
+            .HasKey(ue => ue.Id);
+        // ManyToMany vers Parcours
+        modelBuilder.Entity<Ue>()
+            .HasMany(ue => ue.EnseigneeDans)
+            .WithMany(p => p.UesEnseignees);
+        // OneToMany vers Note
+        modelBuilder.Entity<Ue>()
+            .HasMany(ue => ue.Notes)
+            .WithOne(n => n.Ue);
+        
+        // Propriétés de la table Note
+        // Clé primaire composite
+        modelBuilder.Entity<Notes>()
+            .HasKey(n => new { n.EtudiantId, n.UeId });
+        // ManyToOne vers Etudiant
+        modelBuilder.Entity<Notes>()
+            .HasOne(n => n.Etudiant)
+            .WithMany(e => e.NotesObtenues);
+        // ManyToOne vers Ue
+        modelBuilder.Entity<Notes>()
+            .HasOne(n => n.Ue)
+            .WithMany(ue => ue.Notes);
     }
+    
+    public DbSet <Parcours>? Parcours { get; set; }
+    public DbSet <Etudiant>? Etudiants { get; set; }
+    public DbSet <Ue>? Ues { get; set; }
+    public DbSet <Notes>? Notes { get; set; }
 }
