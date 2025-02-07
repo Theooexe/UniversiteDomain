40eb8e73601a6357873918bf951d5a61cfc14097
Theoode <theoquennehenpro@gmail.com>
2025-02-07 11:14:23 +0100

    Modis de Program.cs 7.10

--- UniversityRestApi/Program.cs
+++ UniversityRestApi/Program.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol.Core.Types;
 using UniversiteDomain.DataAdapters;
 using UniversiteDomain.DataAdapters.DataAdaptersFactory;
 using UniversiteDomain.JeuxDeDonnees;
 using UniversiteEFDataProvider.Data;
+using UniversiteEFDataProvider.Entities;
 using UniversiteEFDataProvider.RepositoryFactories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,6 +25,16 @@ String connectionString = builder.Configuration.GetConnectionString("MySqlConnec
 builder.Services.AddDbContext<UniversiteDbContext>(options =>options.UseMySQL(connectionString));
 builder.Services.AddScoped<IRepositoryFactory, RepositoryFactory>();
 
+// Sécurisation
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddCookie(IdentityConstants.ApplicationScheme)
+    .AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services.AddIdentityCore<UniversiteUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<UniversiteDbContext>() // Ici, on stocke les users dans la même bd que le reste
+    .AddApiEndpoints();
 
 var app = builder.Build();
 
@@ -33,6 +45,10 @@ app.MapControllers();
 // Commentez les deux lignes ci-dessous pour désactiver Swagger (en production par exemple)
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseAuthorization();
+// Ajoute les points d'entrée dans l'API pour s'authentifier, se connecter et se déconnecter
+app.MapIdentityApi<UniversiteUser>();
 
 /*
 using(var scope = app.Services.CreateScope())
@@ -49,8 +65,6 @@ using(var scope = app.Services.CreateScope())
     logger.LogInformation("Création de la BD et des tables à partir des entities");
     await context.Database.EnsureCreatedAsync();
 }
-*/
-
 
 // Initisation de la base de données
 ILogger loggers = app.Services.GetRequiredService<ILogger<BdBuilder>>();
@@ -63,6 +77,8 @@ using(var scope = app.Services.CreateScope())
     BdBuilder seedBD = new BasicBdBuilder(repositoryFactory);
     await seedBD.BuildUniversiteBdAsync();
 }
+*/
+
 
 
 app.Run();
