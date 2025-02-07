6d6e59c79167099f02324d476640812559838f98
Theoode <theoquennehenpro@gmail.com>
2025-02-07 09:23:08 +0100

    5.6

--- UniversityRestApi/Controllers/EtudiantController.cs
+++ UniversityRestApi/Controllers/EtudiantController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UniversiteDomain.DataAdapters.DataAdaptersFactory;
+using UniversiteDomain.Dtos;
 using UniversiteDomain.Entities;
 using UniversiteDomain.UseCases.EtudiantUseCases.Create;
 
@@ -25,11 +26,17 @@ namespace UniversityRestApi.Controllers
         }
         
 
+        // POST api/<EtudiantController>
         [HttpPost]
-        public async Task<Etudiant> PostAsync([FromBody] Etudiant etudiant)
+        public async Task<EtudiantDto> PostAsync([FromBody] EtudiantDto etudiantDto)
         {
+            // Converstion d'un EtudiantDto sans Id en Etudiant
+            Etudiant etudiant = etudiantDto.ToEntity();
+            // Création d'un étudiant
             CreateEtudiantUseCase uc=new CreateEtudiantUseCase(repositoryFactory);
-            return await uc.ExecuteAsync(etudiant);
+            etudiant = await uc.ExecuteAsync(etudiant);
+            // Conversion de l'étudiant avec son nouvel Id en EtudiantDto
+            return new EtudiantDto().ToDto(etudiant);
         }
 
         // PUT api/<EtudiantController>/5
