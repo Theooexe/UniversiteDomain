e641218a5658fd5e096755456c1f0b05242c0b44
Theoode <theoquennehenpro@gmail.com>
2025-02-07 10:58:26 +0100

    Modifs du dbcontext 7.8

--- UniversityRestApi/Controllers/EtudiantController.cs
+++ UniversityRestApi/Controllers/EtudiantController.cs
@@ -37,13 +37,10 @@ namespace UniversityRestApi.Controllers
             }
             catch (Exception e)
             {
-                // On récupère ici les exceptions personnalisées définies dans la couche domain
-                // Et on les envoie avec le code d'erreur 400 et l'intitulé "erreurs de validation"
                 ModelState.AddModelError(nameof(e), e.Message);
                 return ValidationProblem();
             }
             EtudiantDto dto = new EtudiantDto().ToDto(etud);
-            // On revoie la route vers le get qu'on n'a pas encore écrit!
             return CreatedAtAction(nameof(GetUnEtudiant), new { id = dto.Id }, dto);
         }
 
